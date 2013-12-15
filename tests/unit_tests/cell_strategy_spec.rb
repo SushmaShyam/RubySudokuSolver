b0f3a1061219b197b32e004be144057dd017c0e9
SushmaShyam <sushma.shyam@gmail.com>
2013-12-15 17:52:01 +0530

    more tests and invalid puzzle test added

---
+++ tests/unit_tests/cell_strategy_spec.rb
@@ -0,0 +1,42 @@
+=begin
++The MIT License (MIT)
++
++Copyright (c) 2013 SushmaShyam
++
++Permission is hereby granted, free of charge, to any person obtaining a copy of
++this software and associated documentation files (the "Software"), to deal in
++the Software without restriction, including without limitation the rights to
++use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
++the Software, and to permit persons to whom the Software is furnished to do so,
++subject to the following conditions:
++  
+=end
+require_relative '../../lib/sudoku_grid.rb'
+VALIDSET = Set['1','2','3','4','5','6','7','8','9']
+describe "CellStrategy" do
+  context "valid puzzle" do
+    before(:each) do
+      
+      @file = File.new("puzzles/puzzle_easy.txt")
+      @grid = SudokuGrid.new(@file)
+      @strategy = CellStrategy.new(@grid) 
+    end
+    it "should solve a valid puzzle" do
+       @strategy.solve == true
+    end
+  end
+    
+  context "invalid puzzle" do
+    before(:each) do
+      
+      @file = File.new("puzzles/puzzle_invalid.txt")
+      @grid = SudokuGrid.new(@file)
+      @strategy = CellStrategy.new(@grid) 
+    end
+    it "should not solve an invalid puzzle" do
+       @strategy.solve == false
+    end
+  end
+  
+end
+
--- tests/unit_tests/sudoku_grid_spec.rb
+++ tests/unit_tests/sudoku_grid_spec.rb
@@ -1,5 +1,19 @@
+=begin
++The MIT License (MIT)
++
++Copyright (c) 2013 SushmaShyam
++
++Permission is hereby granted, free of charge, to any person obtaining a copy of
++this software and associated documentation files (the "Software"), to deal in
++the Software without restriction, including without limitation the rights to
++use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
++the Software, and to permit persons to whom the Software is furnished to do so,
++subject to the following conditions:
++  
+=end
+
 require_relative '../../lib/sudoku_grid.rb'
-require 'rspec/given'
+
 VALIDSET = Set['1','2','3','4','5','6','7','8','9']
 describe 'SudokuGrid' do
   before(:all) do
@@ -23,10 +37,19 @@ describe 'SudokuGrid' do
     @grid.unsolvable? == false
   end
   
-  it "should be solved" do
-    @grid.solve
-    @grid.solved? == true
+  context "after solving" do
+    before(:each) do
+      @grid.solve  
+    end
+    it "should be solved" do    
+      @grid.solved? == true
+    end
+    it "should have correct state" do
+      @grid.state == ["3", "2", "5","6", "1", "8","4", "9", "7","7", "1", "6","9", "2", "4","3", "8", "5","9", "8", "4","5", "3", "7","6", "1", "2","5", "6", "9","4", "8", "2","7", "3", "1",
+      "1", "4", "2", "3", "7", "5", "8", "6", "9", "8", "3", "7", "1", "6", "9", "2", "5", "4","4", "7", "3", "8", "5", "1", "9", "2", "6","2", "5", "8","7", "9", "6", "1", "4", "3","6", "9", "1", "2", "4", "3","5", "7", "8"]
+    end
   end
+
   after(:all) do
     @file.close
   end
